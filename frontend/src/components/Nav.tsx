7b10f99e6f493d2e1a2b254e4f5ecbdcc36e81ab
Jordan Barone <j.barone024@gmail.com>
2026-08-05 14:39:46 -0400

    Add SlotSync icon for browser tab and nav

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -11,7 +11,10 @@ export function Nav() {
 
   return (
     <nav className="nav">
-      <Link to="/" className="nav-brand">SlotSync</Link>
+      <Link to="/" className="nav-brand">
+        <img src="/favicon.svg" alt="" width="20" height="20" style={{ verticalAlign: "middle", marginRight: 8 }} />
+        SlotSync
+      </Link>
       <div className="nav-links">
         {isLoggedIn ? (
           <>
